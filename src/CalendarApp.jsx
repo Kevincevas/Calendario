45d1a9136c3668408d05d55547d881bca794e1e9
“Kevincevas” <cevas102@gmail.com>
2023-08-19 21:09:35 -0500

    update

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
@@ -9,7 +9,10 @@ export const CalendarApp = () => {
 
     <Provider store={ store }>
       <BrowserRouter>
+      {/* una manera de solucionar el problema de rutas, no es recomendado utilizar, mejor hacer el arreglo en el backend */}
+      {/* <HashRouter> */}
         <AppRouter />
+      {/* </HashRouter> */}
       </BrowserRouter>
     </Provider>
   )
